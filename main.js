f7802e425a7e800e5fab463a9974ea502a67b25b
shaunhailey <ediblelysol@gmail.com>
2017-08-13 16:12:28 -0400

    still not displaying on index html

--- main.js
+++ main.js
@@ -14,9 +14,39 @@ button.addEventListener('click', () => {
     console.log(data.results)
     for (var i = 0; i < data.length; i++) {
       let item = data[i]
-      document.createElement('li')
-      list.appendChild(li)
-      let liContent = document.createTextNode.data.item
+      let list = document.createElement('li')
+      let image = document.createElement('img')
+      let anchor = document.createElement('anchor')
+      list.appendChild(list)
+      let listContent = document.createTextNode.data.item
+      anchor.href = data.item.href
+      list.textContent = data.item.title
+      image.src = data.item.thumbnail
+      if (data.item.thumbnail === '') {
+        image.src = 'http://via.placeholder.com/100x100'
+      }
+      section.appendChild(uls)
+      anchor.appendChild(anchor)
+      list.appendChild(list)
+      image.appendChild(image)
     }
   })
 })
+
+// let uls = document.createElement('ul')
+//         let li = document.createElement('li')
+//         let liNail = document.createElement('img')
+//         let a = document.createElement('a')
+//         a.href = data.results[i].href
+//         li.textContent = data.results[i].title
+//         liNail.src = data.results[i].thumbnail
+//           if (data.results[i].thumbnail === '') {
+//             liNail.src = 'http://via.placeholder.com/100x100'
+//           }
+//         section.appendChild(uls)
+//         uls.appendChild(a)
+//         a.appendChild(li)
+//         li.appendChild(liNail)
+//       }
+//     })
+// })
